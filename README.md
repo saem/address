52439bf2abd2a3b68b08b97391858bd52a3d0d9f
nemitek <mkotlikov@gmail.com>
2015-01-18 23:41:57 +0000

    January 18th 2015 end of day commit.

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Glorified address book
 ```
 psql address -h 127.0.0.1 -d address
 ```
+
+# todos
+* [Create relationship tables between entities and emails/phones/address]()
+** Do we deal with emails with emails
+* Figure out the axes of various properties, actor/non-actor, fundamental entity type, intrinsic vs extrinsic
+** Emails: We don't know who clicked on the unsubscribe link -> extrinsic or system, actor or non-actor??
+* Actor: Entities, Email?
+* Non-Actor: Person, Email?, Address, Phone Number
--- src/main/java/opus/address/AddressApplication.java
+++ src/main/java/opus/address/AddressApplication.java
@@ -6,6 +6,9 @@ import io.dropwizard.db.ManagedDataSource;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.joda.time.DateTimeZone;
+
+import java.util.TimeZone;
 
 public class AddressApplication extends Application<AddressConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -25,6 +28,11 @@ public class AddressApplication extends Application<AddressConfiguration> {
 
     @Override
     public void run(AddressConfiguration cfg, Environment env) throws Exception {
+        // Set Joda Time Zone to UTC
+        DateTimeZone.setDefault(DateTimeZone.UTC);
+        // Set Java Time Zone to UTC
+        TimeZone.setDefault(DateTimeZone.UTC.toTimeZone());
+
         final ManagedDataSource ds = cfg.getDataSourceFactory().build(env.metrics(), "db");
     }
 }
